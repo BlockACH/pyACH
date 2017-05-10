cdd3bc3a5471f35e3c9ba3406ac1e433c8c7b5eb
boolafish <boolafish945@gmail.com>
2017-05-10 07:00:48 +0000

    change bank_id to url

--- api/smart_contract_views.py
+++ api/smart_contract_views.py
@@ -14,18 +14,16 @@ def index():
     return 'Smart Contract!'
 
 
-@smart_contract.route('/notify', methods=['POST'])
-def notify():
-    bank_id = request.args.get('bank_id', '')
+@smart_contract.route('/<bank_id>/notify', methods=['POST'])
+def notify(bank_id):
     data = request.json
     presenter = NotificationPresenter(bank_id, SMART_CONTRACT_MODEL)
     key = presenter.notify(data)
     return jsonify(data={'key': key})
 
 
-@smart_contract.route('/transaction/query', methods=['GET'])
-def query():
-    bank_id = request.args.get('bank_id', '')
+@smart_contract.route('/<bank_id>/transaction/query', methods=['GET'])
+def query(bank_id):
     trigger_bank = request.args.get('t', '')
     receive_bank = request.args.get('r', '')
     presenter = TransactionPresenter(bank_id, SMART_CONTRACT_MODEL)
@@ -33,27 +31,24 @@ def query():
     return jsonify(data=txs)
 
 
-@smart_contract.route('/transaction/ready', methods=['POST'])
-def ready():
-    bank_id = request.args.get('bank_id', '')
+@smart_contract.route('/<bank_id>/transaction/ready', methods=['POST'])
+def ready(bank_id):
     data = request.json
     presenter = TxStateChangePresenter(bank_id, SMART_CONTRACT_MODEL)
     tx_data = presenter.ready(data)
     return jsonify(data=tx_data)
 
 
-@smart_contract.route('/transaction/accept', methods=['POST'])
-def accept():
-    bank_id = request.args.get('bank_id', '')
+@smart_contract.route('/<bank_id>/transaction/accept', methods=['POST'])
+def accept(bank_id):
     data = request.json
     presenter = TxStateChangePresenter(bank_id, SMART_CONTRACT_MODEL)
     tx_data = presenter.accept(data['key'])
     return jsonify(data=tx_data)
 
 
-@smart_contract.route('/transaction/reject', methods=['POST'])
-def reject():
-    bank_id = request.args.get('bank_id', '')
+@smart_contract.route('/<bank_id>/transaction/reject', methods=['POST'])
+def reject(bank_id):
     data = request.json
     presenter = TxStateChangePresenter(bank_id, SMART_CONTRACT_MODEL)
     tx_data = presenter.reject(data['key'])
--- runserver.py
+++ runserver.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask
+from flask import Flask, url_for, jsonify
 
 from api.settle_views import settle
 from api.smart_contract_views import smart_contract
@@ -8,6 +8,29 @@ from api.smart_contract_views import smart_contract
 app = Flask(__name__)
 app.register_blueprint(settle, url_prefix='/settle')
 app.register_blueprint(smart_contract, url_prefix='/smart_contract')
+
+
+@app.route('/')
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(
+            rule.endpoint, methods, url
+        ))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
+    return jsonify(data=output)
+
 
 if len(sys.argv) > 1:
     port = int(sys.argv[1])
