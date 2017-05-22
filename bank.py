d4b7de29e6f2dcaf18d7629de4a8708f32951684
boolafish <boolafish945@gmail.com>
2017-05-22 06:35:24 +0000

    handle error msg & fix `tch` --> `TCH`

--- bank.py
+++ bank.py
@@ -19,6 +19,7 @@ class BankManager(object):
         self.bank_list = BANK_LIST
         self.bank_list.append('central_bank')
         self.bank_cache = {}
+        self._tch = None
         self._central_bank = None
         self._tch = None
         self.tx_pool = []
